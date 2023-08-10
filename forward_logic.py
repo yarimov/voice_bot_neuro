f9f2cd30e9cf8f846678436397060d63ff69f36c
Alex <yarimov@ya.ru>
2023-08-10 14:21:21 +0300

    Создана структура forward_logic

---
+++ forward_logic.py
@@ -0,0 +1,19 @@
+from libraries import NeuroVoiceLibrary
+
+
+def forward(nv: NeuroVoiceLibrary) -> str:
+    pass
+
+
+def end_logic(tag) -> dict:
+    pass
+
+
+def run(nv: NeuroVoiceLibrary, question: bool = False) -> dict:
+    pass
+
+
+if __name__ == "__main__":
+    neuro_voice = NeuroVoiceLibrary()
+    res = run()
+    print(res)
