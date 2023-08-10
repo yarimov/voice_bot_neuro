7dd1503ff03d1797d8193da78028a046199718a0
Alex <yarimov@ya.ru>
2023-08-10 13:36:05 +0300

    Создан структура hello_logic

---
+++ hello_logic.py
@@ -0,0 +1,36 @@
+from libraries import NeuroNetLibrary, NeuroVoiceLibrary
+
+
+def hello():
+    pass
+
+
+def hello_null():
+    pass
+
+
+def hello_repeat():
+    pass
+
+
+def has_reaction_by_answer():
+    pass
+
+
+def map_resolver():
+    pass
+
+
+def end_logic():
+    pass
+
+
+def run():
+    pass
+
+
+if __name__ == "__main__":
+    neuro_net = NeuroNetLibrary()
+    neuro_voice = NeuroVoiceLibrary()
+    res = run()
+    print(res)
